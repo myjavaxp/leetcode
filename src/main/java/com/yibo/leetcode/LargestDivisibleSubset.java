1981a3cc42790fb3c6f24e92ea64c88aee79e224
Yibo <xuyibo@outlook.com>
2018-07-10 13:59:51 +0800

    一道新题目

---
+++ src/main/java/com/yibo/leetcode/LargestDivisibleSubset.java
@@ -0,0 +1,28 @@
+package com.yibo.leetcode;
+
+import java.util.List;
+
+/**
+ * 给出一个由无重复的正整数组成的集合, 找出其中最大的整除子集, 子集中任意一对 (Si, Sj) 都要满足: Si % Sj = 0 或 Sj % Si
+ * = 0。
+ * 
+ * 如果有多个目标子集，返回其中任何一个均可。
+ * 
+ * 示例 1:
+ * 
+ * 集合: [1,2,3]
+ * 
+ * 结果: [1,2] (当然, [1,3] 也正确) 示例 2:
+ * 
+ * 集合: [1,2,4,8]
+ * 
+ * 结果: [1,2,4,8]
+ * 
+ * @author Yibo
+ *
+ */
+public class LargestDivisibleSubset {
+	public List<Integer> largestDivisibleSubset(int[] nums) {
+		return null;
+	}
+}
