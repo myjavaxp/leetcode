1bef696101f533222211efad4f525c5cbd55d237
Yibo <xuyibo@outlook.com>
2018-07-12 11:13:16 +0800

    错误解法

---
+++ src/main/java/com/yibo/leetcode/DivisibleSubset.java
@@ -0,0 +1,51 @@
+package com.yibo.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class DivisibleSubset {
+	public List<Integer> largestDivisibleSubset(int[] nums) {
+		if (nums.length < 2) {
+			List<Integer> temp = new ArrayList<>();
+			for (int n : nums) {
+				temp.add(n);
+			}
+			return temp;
+		}
+
+		Arrays.sort(nums);
+		Map<Integer, List<Integer>> map = new HashMap<>();
+		map.put(nums[0], Arrays.asList(nums[0]));
+		for (int i = 0; i < nums.length; i++) {
+			List<Integer> list = calculate(map, nums[i]);
+			map.put(nums[i], list);
+		}
+		int max = 0;
+		List<Integer> result = new ArrayList<>();
+		for (Integer n : map.keySet()) {
+			List<Integer> list = map.get(n);
+			if (list.size() > max) {
+				max = list.size();
+				result = list;
+			}
+		}
+		return result;
+	}
+
+	private List<Integer> calculate(Map<Integer, List<Integer>> map, int k) {
+		Set<Integer> result = new HashSet<>();
+		Set<Integer> keySet = map.keySet();
+		for (Integer integer : keySet) {
+			if (k % integer == 0) {
+				result.add(integer);
+			}
+		}
+		result.add(k);
+		return new ArrayList<>(result);
+	}
+}
--- src/test/java/com/yibo/test/TestMaxDistance.java
+++ src/test/java/com/yibo/test/TestMaxDistance.java
@@ -1,54 +1,69 @@
 package com.yibo.test;
 
-import com.yibo.leetcode.*;
-import org.junit.Test;
-
 import java.util.List;
 
+import org.junit.Test;
+
+import com.yibo.leetcode.BiggestNum;
+import com.yibo.leetcode.DivisibleSubset;
+import com.yibo.leetcode.IncreaseSequence;
+import com.yibo.leetcode.LemonadeChange;
+import com.yibo.leetcode.LongestPalindrome;
+import com.yibo.leetcode.MaxDistance;
+import com.yibo.leetcode.PerfectNumber;
+import com.yibo.leetcode.ReverseWord;
+
 public class TestMaxDistance {
-    @Test
-    public void test01() {
-        int i = MaxDistance.maxDistToClosest(new int[]{0, 1, 0, 0, 0, 0});
-        System.out.println(i);
-    }
+	@Test
+	public void test01() {
+		int i = MaxDistance.maxDistToClosest(new int[] { 0, 1, 0, 0, 0, 0 });
+		System.out.println(i);
+	}
 
-    @Test
-    public void test02() {
-        IncreaseSequence increaseSequence = new IncreaseSequence();
-        List<List<Integer>> list = increaseSequence.findSubsequences(
-                new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
-        list.forEach(System.out::println);
-        System.out.println(list.size());
-    }
+	@Test
+	public void test02() {
+		IncreaseSequence increaseSequence = new IncreaseSequence();
+		List<List<Integer>> list = increaseSequence
+				.findSubsequences(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
+		list.forEach(System.out::println);
+		System.out.println(list.size());
+	}
 
-    @Test
-    public void test03() {
-        String s = ReverseWord.reverseWords("the best football       ");
-        System.out.println(s);
-        System.out.println(s.length());
-    }
+	@Test
+	public void test03() {
+		String s = ReverseWord.reverseWords("the best football       ");
+		System.out.println(s);
+		System.out.println(s.length());
+	}
 
-    @Test
-    public void test04() {
-        LemonadeChange lemonadeChange = new LemonadeChange();
-        System.out.println(lemonadeChange.lemonadeChange(new int[]{10}));
-    }
+	@Test
+	public void test04() {
+		LemonadeChange lemonadeChange = new LemonadeChange();
+		System.out.println(lemonadeChange.lemonadeChange(new int[] { 10 }));
+	}
 
-    @Test
-    public void test05() {
-        LongestPalindrome longestPalindrome = new LongestPalindrome();
-        String palindrome = longestPalindrome.longestPalindrome("abcbaabcdcba");
-        System.out.println(palindrome);
-    }
+	@Test
+	public void test05() {
+		LongestPalindrome longestPalindrome = new LongestPalindrome();
+		String palindrome = longestPalindrome.longestPalindrome("abcbaabcdcba");
+		System.out.println(palindrome);
+	}
 
-    @Test
-    public void test06() {
-        PerfectNumber perfectNumber = new PerfectNumber();
-        System.out.println(perfectNumber.checkPerfectNumber(28));
-    }
+	@Test
+	public void test06() {
+		PerfectNumber perfectNumber = new PerfectNumber();
+		System.out.println(perfectNumber.checkPerfectNumber(28));
+	}
 
-    @Test
-    public void test07() {
-        System.out.println(BiggestNum.gcd(6, 15));
-    }
+	@Test
+	public void test07() {
+		System.out.println(BiggestNum.gcd(6, 15));
+	}
+
+	@Test
+	public void test08() {
+		DivisibleSubset divisibleSubset = new DivisibleSubset();
+		int[] nums = new int[] { 4,8,10,240 };
+		System.out.println(divisibleSubset.largestDivisibleSubset(nums));
+	}
 }
