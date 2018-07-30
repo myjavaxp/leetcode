94c54e36e8c5a8dcdbfc24ee1e4797ebccd36523
XuYibo <xuyibo@outlook.com>
2018-07-30 13:25:48 +0800

    矩形重复

--- src/main/java/com/yibo/leetcode/RectangleOverlap.java
+++ src/main/java/com/yibo/leetcode/RectangleOverlap.java
@@ -1,8 +1,5 @@
 package com.yibo.leetcode;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * 矩形以列表 [x1, y1, x2, y2] 的形式表示，其中 (x1, y1) 为左下角的坐标，(x2, y2) 是右上角的坐标。
  * <p>
@@ -12,35 +9,27 @@ import java.util.List;
  * <p>
  * 示例 1：
  * <p>
- * 输入：rec1 = [0,0,2,2], rec2 = [1,1,3,3]
- * 输出：true
- * 示例 2：
+ * 输入：rec1 = [0,0,2,2], rec2 = [1,1,3,3] 输出：true 示例 2：
  * <p>
- * 输入：rec1 = [0,0,1,1], rec2 = [1,0,2,1]
- * 输出：false
- * 说明：
+ * 输入：rec1 = [0,0,1,1], rec2 = [1,0,2,1] 输出：false 说明：
  * <p>
- * 两个矩形 rec1 和 rec2 都以含有四个整数的列表的形式给出。
- * 矩形中的所有坐标都处于 -10^9 和 10^9 之间。
+ * 两个矩形 rec1 和 rec2 都以含有四个整数的列表的形式给出。 矩形中的所有坐标都处于 -10^9 和 10^9 之间。
  * https://leetcode-cn.com/problems/rectangle-overlap/description/
  */
 public class RectangleOverlap {
-    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        List<int[]> point1 = getAllPoint(rec1);
-        List<int[]> point2 = getAllPoint(rec2);
-        //三不沾
-        return false;
-    }
-
-    private boolean outOfTheRec(int[] p, int[] rec) {
-        return p[0] < rec[0] || p[0] > rec[2] || p[1] < rec[1] || p[1] > rec[3];
-    }
-
-    private List<int[]> getAllPoint(int[] rec) {
-        int[] a = {rec[0], rec[1]};
-        int[] b = {rec[2], rec[3]};
-        int[] c = {rec[0], rec[3]};
-        int[] d = {rec[2], rec[1]};
-        return Arrays.asList(a, b, c, d);
-    }
+	public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+		if (rec1[0] >= rec2[2]) {
+			return false;
+		}
+		if (rec2[0] >= rec1[2]) {
+			return false;
+		}
+		if (rec1[1] >= rec2[3]) {
+			return false;
+		}
+		if (rec2[1] >= rec1[3]) {
+			return false;
+		}
+		return true;
+	}
 }
