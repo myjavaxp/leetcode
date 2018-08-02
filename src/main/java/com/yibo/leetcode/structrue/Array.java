b29fec92b3f31f554e01abb02060ccd291ffcd4b
Yibo <xuyibo@outlook.com>
2018-08-02 14:40:50 +0800

    自定义数组

--- src/main/java/com/yibo/leetcode/structrue/Array.java
+++ src/main/java/com/yibo/leetcode/structrue/Array.java
@@ -3,88 +3,156 @@ package com.yibo.leetcode.structrue;
 import java.io.Serializable;
 
 public class Array implements Serializable {
-	private static final long serialVersionUID = -3926745631703053617L;
-	private int[] data;
-	private int size;
+    private static final long serialVersionUID = -3926745631703053617L;
+    private int[] data;
+    private int size;
 
-	/**
-	 * 构造函数
-	 * 
-	 * @param capacity 数组最大容量
-	 */
-	public Array(int capacity) {
-		data = new int[capacity];
-		size = 0;
-	}
+    /**
+     * 构造函数
+     *
+     * @param capacity 数组最大容量
+     */
+    public Array(int capacity) {
+        data = new int[capacity];
+        size = 0;
+    }
 
-	/**
-	 * 无参构造函数
-	 */
-	public Array() {
-		this(10);
-	}
+    /**
+     * 无参构造函数
+     */
+    public Array() {
+        this(10);
+    }
 
-	/**
-	 * 获取元素个数
-	 * 
-	 * @return 元素个数
-	 */
-	public int getSize() {
-		return size;
-	}
+    /**
+     * 获取元素个数
+     *
+     * @return 元素个数
+     */
+    public int size() {
+        return size;
+    }
 
-	/**
-	 * 获取Array容量
-	 * 
-	 * @return Array 容量
-	 */
-	public int getCapacity() {
-		return data.length;
-	}
+    /**
+     * 获取Array容量
+     *
+     * @return Array 容量
+     */
+    public int getCapacity() {
+        return data.length;
+    }
 
-	/**
-	 * 是否是空Array
-	 * 
-	 * @return 是否是空Array
-	 */
-	public boolean isEmpty() {
-		return size == 0;
-	}
+    /**
+     * 是否是空Array
+     *
+     * @return 是否是空Array
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-	public void addFirst(int e) {
-		add(0, e);
-	}
+    public void addFirst(int e) {
+        add(0, e);
+    }
 
-	public void addLast(int e) {
-		add(size, e);
-	}
+    public void addLast(int e) {
+        add(size, e);
+    }
 
-	public void add(int index, int e) {
-		if (size == getCapacity()) {
-			throw new IllegalArgumentException("Add failed. Array is full.");
-		}
-		if (index < 0 || index > size) {
-			throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size");
-		}
-		for (int i = size - 1; i >= index; i--) {
-			data[i + 1] = data[i];
-		}
-		data[index] = e;
-		size++;
-	}
+    public void add(int index, int e) {
+        if (size == getCapacity()) {
+            throw new IllegalArgumentException("Add failed. Array is full.");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size");
+        }
+        if (size - index >= 0) {
+            System.arraycopy(data, index, data, index + 1, size - index);
+        }
+        data[index] = e;
+        size++;
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder res = new StringBuilder();
-		res.append(String.format("Array: size = %d, capacity = %d\n", size, data.length));
-		res.append('[');
-		for (int i = 0; i < size; i++) {
-			res.append(data[i]);
-			if (i != size - 1) {
-				res.append(", ");
-			}
-		}
-		res.append(']');
-		return res.toString();
-	}
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. Require index >= 0 and index < size");
+        }
+        return data[index];
+    }
+
+    public void set(int index, int e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed. Require index >= 0 and index < size");
+        }
+        data[index] = e;
+    }
+
+    public boolean contains(int e) {
+        for (int i = 0; i < size; i++) {
+            if (e == data[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int find(int e) {
+        for (int i = 0; i < size; i++) {
+            if (e == data[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Remove failed. Require index >= 0 and index < size");
+        }
+        int result = data[index];
+        if (size - index + 1 >= 0) {
+            System.arraycopy(data, index + 1, data, index, size - index + 1);
+        }
+        size--;
+        return result;
+    }
+
+    public int removeFirst() {
+        return remove(0);
+    }
+
+    public int removeLast() {
+        return remove(size - 1);
+    }
+
+    public boolean removeElement(int e) {
+        int i = find(e);
+        if (i != -1) {
+            remove(i);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void removeAllElement(int e) {
+        if (removeElement(e)) {
+            removeAllElement(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d, capacity = %d\n", size, data.length));
+        res.append('[');
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if (i != size - 1) {
+                res.append(", ");
+            }
+        }
+        res.append(']');
+        return res.toString();
+    }
 }
--- src/test/java/com/yibo/test/TestArray.java
+++ src/test/java/com/yibo/test/TestArray.java
@@ -1,16 +1,49 @@
 package com.yibo.test;
 
+import com.yibo.leetcode.structrue.Array;
 import org.junit.Test;
 
-import com.yibo.leetcode.structrue.Array;
-
 public class TestArray {
-	@Test
-	public void test01() {
-		Array array = new Array(20);
-		for (int i = 0; i < 11; i++) {
-			array.addLast(i);
-		}
-		System.out.println(array);
-	}
+    @Test
+    public void test01() {
+        Array array = new Array(20);
+        for (int i = 0; i < 11; i++) {
+            array.addLast(i);
+        }
+        System.out.println(array);
+        array.add(0, 99);
+        System.out.println(array);
+        System.out.println(array.getCapacity());
+        System.out.println(array.get(1));
+        System.out.println(array.get(11));
+        array.set(11, 98);
+        System.out.println(array.get(11));
+        System.out.println(array.contains(183291));
+        System.out.println(array.find(1321));
+        System.out.println(array.removeFirst());
+        System.out.println(array);
+        System.out.println(array.removeLast());
+        System.out.println(array);
+        array.set(0,1);
+        array.set(1,1);
+        array.set(2,1);
+        array.set(3,1);
+        array.set(8,1);
+        System.out.println(array);
+        array.removeAllElement(1);
+        System.out.println(array);
+        array.remove(0);
+        System.out.println(array);
+        System.out.println(array.size());
+        System.out.println(array.removeElement(4));
+        Array arr=new Array();
+        System.out.println(arr.isEmpty());
+        arr.addFirst(123);
+        arr.addFirst(1);
+        arr.addFirst(1);
+        arr.addFirst(1);
+        arr.addFirst(1);
+        arr.addFirst(2);
+        System.out.println(arr);
+    }
 }
