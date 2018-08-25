f901351a39c9df8a5b16c8a37d729e5148a0aea9
Yibo <xuyibo@outlook.com>
2018-08-25 15:39:48 +0800

    并查集简单实现--基于数组

--- src/main/java/com/yibo/leetcode/structrue/UnionFind.java
+++ src/main/java/com/yibo/leetcode/structrue/UnionFind.java
@@ -1,9 +1,22 @@
 package com.yibo.leetcode.structrue;
 
 public interface UnionFind {
-    boolean isConnected(int p, int q);
+	/**
+	 * 查看两个元素是否所属一个集合
+	 * 
+	 * @param p 元素1
+	 * @param q 元素2
+	 * @return true-同一个集合, false-不同集合
+	 */
+	boolean isConnected(int p, int q);
 
-    void unionElements(int p, int q);
+	/**
+	 * 合并元素p和元素q所在集合
+	 * 
+	 * @param p 元素1
+	 * @param q 元素2
+	 */
+	void unionElements(int p, int q);
 
-    int size();
+	int size();
 }
