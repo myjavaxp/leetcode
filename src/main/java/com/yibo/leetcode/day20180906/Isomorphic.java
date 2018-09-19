1f371eebb9b5fac968e34c8db2fbf9432db2ebcc
XuYibo <xuyibo@outlook.com>
2018-09-19 17:03:28 +0800

    继续解题

--- src/main/java/com/yibo/leetcode/day20180906/Isomorphic.java
+++ src/main/java/com/yibo/leetcode/day20180906/Isomorphic.java
@@ -7,6 +7,19 @@ package com.yibo.leetcode.day20180906;
  */
 public class Isomorphic {
     public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] freq1 = new int[256];
+        int[] freq2 = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+        }
         return false;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
