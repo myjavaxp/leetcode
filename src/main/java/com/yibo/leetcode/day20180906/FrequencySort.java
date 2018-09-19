c8d2e6cf5eab0da80450b0aaa1ccf2da61a4606d
XuYibo <xuyibo@outlook.com>
2018-09-19 15:21:22 +0800

    451号问题

--- src/main/java/com/yibo/leetcode/day20180906/FrequencySort.java
+++ src/main/java/com/yibo/leetcode/day20180906/FrequencySort.java
@@ -1,5 +1,10 @@
 package com.yibo.leetcode.day20180906;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * https://leetcode-cn.com/problems/sort-characters-by-frequency/description/
  *
@@ -7,6 +12,20 @@ package com.yibo.leetcode.day20180906;
  */
 public class FrequencySort {
     public String frequencySort(String s) {
-        return null;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.merge(s.charAt(i), 1, (a, b) -> (a + b));
+        }
+        StringBuilder result = new StringBuilder();
+        Queue<Character> queue = new PriorityQueue<>((o1, o2) -> map.get(o2) - map.get(o1));
+        queue.addAll(map.keySet());
+        while (!queue.isEmpty()) {
+            Character c = queue.poll();
+            Integer num = map.get(c);
+            for (int i = 0; i < num; i++) {
+                result.append(c);
+            }
+        }
+        return result.toString();
     }
 }
