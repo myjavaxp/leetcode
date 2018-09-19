ef56834bc1981e56f7aaf1b3627abe58e7422fa5
XuYibo <xuyibo@outlook.com>
2018-09-19 14:02:45 +0800

    滑动窗口

--- src/main/java/com/yibo/leetcode/day20180905/LongestSubstring.java
+++ src/main/java/com/yibo/leetcode/day20180905/LongestSubstring.java
@@ -2,21 +2,26 @@ package com.yibo.leetcode.day20180905;
 
 /**
  * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
- * 
+ *
  * @author Yibo
  */
 public class LongestSubstring {
-	public int lengthOfLongestSubstring(String s) {
-		int l = 0, r = -1, max = 0;
-		int[] freq = new int[256];
-		while (l < s.length()) {
-			if (r + 1 < s.length() && freq[s.charAt(r + 1)] == 0) {
-				freq[s.charAt(++r)]++;
-			} else {
-				freq[s.charAt(l++)]--;
-			}
-			max = Math.max(max, r - l + 1);
-		}
-		return max;
-	}
+    public int lengthOfLongestSubstring(String s) {
+        int l = 0, r = -1, max = 0;
+        int[] freq = new int[256];
+        while (l < s.length()) {
+            if (r + 1 < s.length() && freq[s.charAt(r + 1)] == 0) {
+                freq[s.charAt(++r)]++;
+            } else {
+                freq[s.charAt(l++)]--;
+            }
+            max = Math.max(max, r - l + 1);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        LongestSubstring longestSubstring = new LongestSubstring();
+        System.out.println(longestSubstring.lengthOfLongestSubstring("abcabcbb"));
+    }
 }
