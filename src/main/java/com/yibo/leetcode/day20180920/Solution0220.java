5809c8f227b8ffc83b0df6125d85c4560a6bb04f
XuYibo <xuyibo@outlook.com>
2018-09-20 15:50:20 +0800

    TreeSet应用

---
+++ src/main/java/com/yibo/leetcode/day20180920/Solution0220.java
@@ -0,0 +1,20 @@
+package com.yibo.leetcode.day20180920;
+
+import java.util.TreeSet;
+
+public class Solution0220 {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        TreeSet<Long> record = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            Long ceiling = record.ceiling((long) nums[i] - t);
+            if (ceiling != null && ceiling <= (long) nums[i] + t) {
+                return true;
+            }
+            record.add((long) nums[i]);
+            if (record.size() == k + 1) {
+                record.remove((long) nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
