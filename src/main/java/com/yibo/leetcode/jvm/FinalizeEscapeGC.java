75795c109b22d219aedc68cc18f3e1b8a62d50b7
XuYibo <xuyibo@outlook.com>
2018-09-27 14:58:26 +0800

    jvm学习

---
+++ src/main/java/com/yibo/leetcode/jvm/FinalizeEscapeGC.java
@@ -0,0 +1,39 @@
+package com.yibo.leetcode.jvm;
+
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings("deprecation")
+public class FinalizeEscapeGC {
+    private static FinalizeEscapeGC SAVE_HOOK = null;
+
+    private void isAlive() {
+        System.out.println("yes,i'm still alive");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("finalize method executed");
+        FinalizeEscapeGC.SAVE_HOOK = this;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SAVE_HOOK = new FinalizeEscapeGC();
+        SAVE_HOOK = null;
+        System.gc();
+        TimeUnit.SECONDS.sleep(1);
+        if (SAVE_HOOK != null) {
+            SAVE_HOOK.isAlive();
+        } else {
+            System.out.println("no,i'm dead");
+        }
+        SAVE_HOOK = null;
+        System.gc();
+        TimeUnit.SECONDS.sleep(1);
+        if (SAVE_HOOK != null) {
+            SAVE_HOOK.isAlive();
+        } else {
+            System.out.println("no,i'm dead");
+        }
+    }
+}
