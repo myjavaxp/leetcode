0f2883c4220e2636e847fe8bf775a454a0e4f591
XuYibo <xuyibo@outlook.com>
2018-10-16 09:29:53 +0800

    第17题第另外一个解法

--- src/main/java/com/yibo/leetcode/day20180920/Solution0017.java
+++ src/main/java/com/yibo/leetcode/day20180920/Solution0017.java
@@ -1,54 +1,29 @@
 package com.yibo.leetcode.day20180920;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution0017 {
-    public List<String> letterCombinations(String digits) {
-        if (digits.length() < 2) {
-            return getSingle(digits);
+    private List<String> res = new ArrayList<>();
+    private static final String[] LETTER_MAP = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    private void findCombinations(String digits, int index, String s) {
+        if (index == digits.length()) {
+            res.add(s);
+            return;
         }
-        List<String> removeFirst = letterCombinations(digits.substring(1));
-        List<String> first = getSingle(digits.substring(0, 1));
-        Set<String> set = new HashSet<>();
-        for (String s : removeFirst) {
-            for (String f : first) {
-                set.add(f + s);
-            }
+        char c = digits.charAt(index);
+        String letters = LETTER_MAP[c - '0'];
+        for (int i = 0; i < letters.length(); i++) {
+            findCombinations(digits, index + 1, s + letters.charAt(i));
         }
-        return new ArrayList<>(set);
     }
 
-    private List<String> getSingle(String digit) {
-        List<String> list;
-        switch (digit) {
-            case "2":
-                list = Arrays.asList("a", "b", "c");
-                break;
-            case "3":
-                list = Arrays.asList("d", "e", "f");
-                break;
-            case "4":
-                list = Arrays.asList("g", "h", "i");
-                break;
-            case "5":
-                list = Arrays.asList("j", "k", "l");
-                break;
-            case "6":
-                list = Arrays.asList("m", "n", "o");
-                break;
-            case "7":
-                list = Arrays.asList("p", "q", "r", "s");
-                break;
-            case "8":
-                list = Arrays.asList("t", "u", "v");
-                break;
-            case "9":
-                list = Arrays.asList("w", "x", "y", "z");
-                break;
-            default:
-                list = new ArrayList<>();
-                break;
+    public List<String> letterCombinations(String digits) {
+        if (digits.equals("")) {
+            return res;
         }
-        return list;
+        findCombinations(digits, 0, "");
+        return res;
     }
 }
