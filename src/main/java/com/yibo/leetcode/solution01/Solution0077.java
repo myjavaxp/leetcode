d01f9d002e4faf815de544d6e027b6846b8a99bb
XuYibo <xuyibo@outlook.com>
2018-10-17 08:35:50 +0800

    理解递归回溯

--- src/main/java/com/yibo/leetcode/solution01/Solution0077.java
+++ src/main/java/com/yibo/leetcode/solution01/Solution0077.java
@@ -17,11 +17,18 @@ public class Solution0077 {
     private void generateCombinations(int n, int k, int start, List<Integer> c) {
         if (c.size() == k) {
             res.add(new ArrayList<>(c));
+            //System.out.println("获得一个集合:" + c);
             return;
         }
+        /*
+         * 这里的结束条件为，剩余可查找的数字个数为n-i+1个，而容器剩余容量为k-c.size()个
+         * 剩余数字太少将填不满容器，故n-1+1>=k-size()
+         */
         for (int i = start; i <= n - (k - c.size()) + 1; i++) {
             c.add(i);
             generateCombinations(n, k, i + 1, c);
+            //移除容器最后一个元素，然后返回上一层递归循环
+            //System.out.println("从:" + c + "移除元素");
             c.remove(c.size() - 1);
         }
     }
