10cd1b4f55987f1dea5e87f07791aa1e69227c31
Yibo <xuyibo@outlook.com>
2018-10-20 16:03:15 +0800

    旋转数组

--- src/main/java/com/yibo/leetcode/solution01/Solution0189.java
+++ src/main/java/com/yibo/leetcode/solution01/Solution0189.java
@@ -3,23 +3,26 @@ package com.yibo.leetcode.solution01;
 /**
  * l=7
  * 输入: [1,2,3,4,5,6,7] 和 k = 3
- *       1,2,3,1,5,6,7
+ * 1,2,3,1,5,6,7
+ * 1,2,3,1,2,3,4
+ * 1,2,3,4,5,6,4
  * 输出: [5,6,7,1,2,3,4]
  */
 public class Solution0189 {
     public void rotate(int[] nums, int k) {
-        int l=nums.length;
-        if(l<2){
+        int l = nums.length;
+        if (l < 2) {
             return;
         }
-        k=k%l;
-        if(k==0){
+        k = k % l;
+        if (k == 0) {
             return;
         }
-        for(int i=0;i<l;i++){
-            //nums[i]=nums[l-k-i];
-            int temp=nums[i+k];
-            nums[i+k]=nums[i];
+        int[] tail = new int[k];
+        System.arraycopy(nums, l - k, tail, 0, k);
+        for (int i = 0; i < l - k; i++) {
+            nums[l - 1 - i] = nums[l - k - i - 1];
         }
+        System.arraycopy(tail, 0, nums, 0, k);
     }
 }
