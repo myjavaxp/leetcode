fa09988e693a58bc7bbade73214c873ba041aa71
XuYibo <xuyibo@outlook.com>
2018-10-23 15:08:31 +0800

    300题动态规划已解决

--- src/main/java/com/yibo/leetcode/solution01/Solution0300.java
+++ src/main/java/com/yibo/leetcode/solution01/Solution0300.java
@@ -13,11 +13,30 @@ package com.yibo.leetcode.solution01;
  * 可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
  * 你算法的时间复杂度应该为 O(n2) 。
  * 进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
+ * 类似问题376号
  *
  * @author yibo
  */
 public class Solution0300 {
     public int lengthOfLIS(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        //memo[i]表示以nums[i]结尾的最长上升自序列的长度
+        int[] memo = new int[nums.length];
+        //算法复杂度o(n2)
+        for (int i = 0; i < nums.length; i++) {
+            memo[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    memo[i] = Math.max(memo[i], 1 + memo[j]);
+                }
+            }
+        }
+        int res = 1;
+        for (int i : memo) {
+            res = Math.max(i, res);
+        }
+        return res;
     }
 }
