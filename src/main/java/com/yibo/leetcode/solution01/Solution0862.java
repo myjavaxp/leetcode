e50293d38e10c3127f307c44dde26d9a7021e92e
XuYibo <xuyibo@outlook.com>
2018-10-25 12:39:06 +0800

    平方和

---
+++ src/main/java/com/yibo/leetcode/solution01/Solution0862.java
@@ -0,0 +1,31 @@
+package com.yibo.leetcode.solution01;
+
+/**
+ * 返回 A 的最短的非空连续子数组的长度，该子数组的和至少为 K 。
+ * <p>
+ * 如果没有和至少为 K 的非空子数组，返回 -1 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：A = [1], K = 1
+ * 输出：1
+ * 示例 2：
+ * <p>
+ * 输入：A = [1,2], K = 4
+ * 输出：-1
+ * 示例 3：
+ * <p>
+ * 输入：A = [2,-1,2], K = 3
+ * 输出：3
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= A.length <= 50000
+ * -10 ^ 5 <= A[i] <= 10 ^ 5
+ * 1 <= K <= 10 ^ 9
+ */
+public class Solution0862 {
+    public int shortestSubarray(int[] A, int K) {
+        return -1;
+    }
+}
