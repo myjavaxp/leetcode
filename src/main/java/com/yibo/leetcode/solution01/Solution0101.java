2963522858117b79c4a509b7d6407b35114704af
Yibo <xuyibo@outlook.com>
2018-11-13 20:40:41 +0800

    222待解决

---
+++ src/main/java/com/yibo/leetcode/solution01/Solution0101.java
@@ -0,0 +1,42 @@
+package com.yibo.leetcode.solution01;
+
+import com.yibo.leetcode.model.TreeNode;
+
+/**
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * \   \
+ * 3    3
+ *
+ * @author Yibo
+ */
+public class Solution0101 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left);
+    }
+}
