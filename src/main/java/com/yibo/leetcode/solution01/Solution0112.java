41441fb814c925d5251a5c63135b889eb0af1fd6
Yibo <xuyibo@outlook.com>
2018-11-13 21:29:50 +0800

    判断一颗树是否是一颗平衡二叉树

---
+++ src/main/java/com/yibo/leetcode/solution01/Solution0112.java
@@ -0,0 +1,15 @@
+package com.yibo.leetcode.solution01;
+
+import com.yibo.leetcode.model.TreeNode;
+
+public class Solution0112 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
---
+++ src/main/java/com/yibo/leetcode/solution01/Solution0404.java
@@ -0,0 +1,9 @@
+package com.yibo.leetcode.solution01;
+
+import com.yibo.leetcode.model.TreeNode;
+
+public class Solution0404 {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return 0;
+    }
+}
