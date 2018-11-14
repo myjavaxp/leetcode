19bbf8106f568a3edbd5b818d2df851a942912b5
XuYibo <xuyibo@outlook.com>
2018-11-14 09:20:18 +0800

    404解决

--- src/main/java/com/yibo/leetcode/solution01/Solution0404.java
+++ src/main/java/com/yibo/leetcode/solution01/Solution0404.java
@@ -4,6 +4,12 @@ import com.yibo.leetcode.model.TreeNode;
 
 public class Solution0404 {
     public int sumOfLeftLeaves(TreeNode root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+        if (root.left != null && root.left.right == null && root.left.left == null) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
     }
 }
