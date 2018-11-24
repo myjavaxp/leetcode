f64cd7a5964b528ea444c6d2eff67d2abded3a7d
Yibo <xuyibo@outlook.com>
2018-11-24 20:41:40 +0800

    第十一题

--- src/main/java/com/yibo/leetcode/solution01/Solution0011.java
+++ src/main/java/com/yibo/leetcode/solution01/Solution0011.java
@@ -1,11 +1,8 @@
 package com.yibo.leetcode.solution01;
 
-/**
- * 解答错误
- */
 public class Solution0011 {
     public int maxArea(int[] height) {
-        int l = 0, r = 1, max = 0, length = height.length;
+        /*int l = 0, r = 1, max = 0, length = height.length;
         while (r < length) {
             int cur = (r - l) * Math.min(height[l], height[r]);
             int next = (r - l - 1) * Math.min(height[l + 1], height[r]);
@@ -16,6 +13,17 @@ public class Solution0011 {
                 max = Math.max(max, cur);
             }
             r++;
+        }
+        return max;*/
+        int max = 0;
+        for (int i = 0, j = height.length - 1; i < j; ) {
+            if (height[i] > height[j]) {
+                max = Math.max(max, height[j] * (j - i));
+                j--;
+            } else {
+                max = Math.max(max, height[i] * (j - i));
+                i++;
+            }
         }
         return max;
     }
