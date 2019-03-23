0fde603e01ad709f839e80798a8d56d91e589394
Yibo <xuyibo@outlook.com>
2019-03-23 16:31:52 +0800

    Future设计

--- src/main/java/com/yibo/leetcode/juc/chapter8/FutureService.java
+++ src/main/java/com/yibo/leetcode/juc/chapter8/FutureService.java
@@ -1,9 +1,21 @@
 package com.yibo.leetcode.juc.chapter8;
 
+import java.util.function.Consumer;
+
 public class FutureService {
     public <V> Future<V> submit(final FutureTask<V> task) {
         AsyncFuture<V> future = new AsyncFuture<>();
         new Thread(() -> future.done(task.call())).start();
+        return future;
+    }
+
+    public <V> Future<V> submit(final FutureTask<V> task, final Consumer<V> consumer) {
+        AsyncFuture<V> future = new AsyncFuture<>();
+        new Thread(() -> {
+            V result = task.call();
+            future.done(result);
+            consumer.accept(result);
+        }).start();
         return future;
     }
 }
--- src/main/java/com/yibo/leetcode/juc/chapter8/SyncInvoker.java
+++ src/main/java/com/yibo/leetcode/juc/chapter8/SyncInvoker.java
@@ -3,11 +3,10 @@ package com.yibo.leetcode.juc.chapter8;
 import java.util.concurrent.TimeUnit;
 
 public class SyncInvoker {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         FutureService service = new FutureService();
-        Future<String> future = service.submit(SyncInvoker::get);
+        service.submit(SyncInvoker::get, System.out::println);
         System.out.println("做一些其他事情");
-        System.out.println(future.get());
         System.out.println("完成Future");
     }
 
