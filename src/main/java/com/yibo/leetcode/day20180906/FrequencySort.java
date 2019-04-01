795151ebb9cd9cba7be2952c887c3da078946e90
Yibo <xuyibo@outlook.com>
2019-04-01 19:39:32 +0800

    观察者模式，以及read write lock

--- src/main/java/com/yibo/leetcode/day20180906/FrequencySort.java
+++ src/main/java/com/yibo/leetcode/day20180906/FrequencySort.java
@@ -14,17 +14,15 @@ public class FrequencySort {
     public String frequencySort(String s) {
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
-            map.merge(s.charAt(i), 1, (a, b) -> (a + b));
+            map.merge(s.charAt(i), 1, Integer::sum);
         }
         StringBuilder result = new StringBuilder();
         Queue<Character> queue = new PriorityQueue<>((o1, o2) -> map.get(o2) - map.get(o1));
         queue.addAll(map.keySet());
         while (!queue.isEmpty()) {
             Character c = queue.poll();
-            Integer num = map.get(c);
-            for (int i = 0; i < num; i++) {
-                result.append(c);
-            }
+            int num = map.get(c);
+            result.append(String.valueOf(c).repeat(num));
         }
         return result.toString();
     }
