103b900fcc563ee14085d7d61727a5ce7f9170fc
Bob Xu <Bob.Xu@nike.com>
2019-10-22 13:36:06 +0800

    136

--- src/main/java/com/yibo/leetcode/juc/ob/BeautyObserver.java
+++ src/main/java/com/yibo/leetcode/juc/ob/BeautyObserver.java
@@ -1,8 +1,14 @@
 package com.yibo.leetcode.juc.ob;
 
 public class BeautyObserver implements Observer {
+    private String className;
+
+    BeautyObserver(String className) {
+        this.className = className;
+    }
+
     @Override
     public void update(String newState) {
-        System.out.println("观察者观察到新状态: " + newState);
+        System.out.println("观察者观察到新状态: " + className + ": " + newState);
     }
 }
--- src/main/java/com/yibo/leetcode/juc/ob/ObserverDemo.java
+++ src/main/java/com/yibo/leetcode/juc/ob/ObserverDemo.java
@@ -3,7 +3,7 @@ package com.yibo.leetcode.juc.ob;
 public class ObserverDemo {
     public static void main(String[] args) {
         NikeSubject subject = new NikeSubject();
-        Observer observer1 = new BeautyObserver();
+        Observer observer1 = new BeautyObserver(subject.getClass().getName());
         //System.out.println(observer1.getClass().getName());
         //Observer observer2 = new BeautyObserver();
         subject.attach(observer1);
