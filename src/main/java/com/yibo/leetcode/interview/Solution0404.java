c74229fbdc3a6c53b0ca5ce8d4582574cf161054
Bob Xu <Bob.Xu@nike.com>
2020-06-18 15:45:19 +0800

    检查平衡性

---
+++ src/main/java/com/yibo/leetcode/interview/Solution0404.java
@@ -0,0 +1,34 @@
+package com.yibo.leetcode.interview;
+
+import com.yibo.leetcode.model.TreeNode;
+
+/**
+ * https://leetcode-cn.com/problems/check-balance-lcci/
+ */
+public class Solution0404 {
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    public int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        if (node.left == null && node.right == null) {
+            return 1;
+        }
+        int leftHeight = getHeight(node.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        int rightHeight = getHeight(node.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
