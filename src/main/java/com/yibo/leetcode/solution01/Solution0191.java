48d5ebe63e4664bfe82255bc2b328945a5a008f8
Yibo <xuyibo@outlook.com>
2020-06-22 16:07:34 +0800

    位1的个数

---
+++ src/main/java/com/yibo/leetcode/solution01/Solution0191.java
@@ -0,0 +1,12 @@
+package com.yibo.leetcode.solution01;
+
+public class Solution0191 {
+    public int hammingWeight(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum++;
+            n &= (n - 1);
+        }
+        return sum;
+    }
+}
