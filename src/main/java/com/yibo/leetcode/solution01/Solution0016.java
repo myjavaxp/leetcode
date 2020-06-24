3ff938e3fd942b457b7e23c88b380032a1bae21c
Yibo <xuyibo@outlook.com>
2020-06-24 08:27:06 +0800

    368

--- src/main/java/com/yibo/leetcode/solution01/Solution0016.java
+++ src/main/java/com/yibo/leetcode/solution01/Solution0016.java
@@ -1,5 +1,7 @@
 package com.yibo.leetcode.solution01;
 
+import java.util.Arrays;
+
 /**
  * 3 <= nums.length <= 10^3
  * -10^3 <= nums[i] <= 10^3
@@ -7,17 +9,20 @@ package com.yibo.leetcode.solution01;
  */
 public class Solution0016 {
     public int threeSumClosest(int[] nums, int target) {
-        int min = Integer.MAX_VALUE;
-        int res = 0;
+        Arrays.sort(nums);
+        int res = nums[0] + nums[1] + nums[2];
         for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int t = nums[i] + nums[j] + nums[k];
-                    int e = Math.abs(t - target);
-                    if (min > e) {
-                        min = e;
-                        res = t;
-                    }
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (Math.abs(target - res) > Math.abs(target - sum)) {
+                    res = sum;
+                }
+                if (sum < target) {
+                    start++;
+                } else {
+                    end--;
                 }
             }
         }
