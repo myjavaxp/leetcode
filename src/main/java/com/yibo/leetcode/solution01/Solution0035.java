b8b4e5b5e5d4591f4a45bccc3e076ce6a982d522
Bob Xu <Bob.Xu@nike.com>
2020-07-17 10:52:15 +0800

    不同的二叉搜索树

---
+++ src/main/java/com/yibo/leetcode/solution01/Solution0035.java
@@ -0,0 +1,18 @@
+package com.yibo.leetcode.solution01;
+
+public class Solution0035 {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1, ans = n;
+        while (left <= right) {
+            int mid = ((right - left) >> 1) + left;
+            if (target <= nums[mid]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
