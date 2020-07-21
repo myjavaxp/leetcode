c6a5d9c938842e5a724ea88616535d56cbe3d959
Bob Xu <Bob.Xu@nike.com>
2020-07-21 13:27:03 +0800

    不同的二叉搜索树

---
+++ src/main/java/com/yibo/leetcode/solution01/Solution0095.java
@@ -0,0 +1,11 @@
+package com.yibo.leetcode.solution01;
+
+import com.yibo.leetcode.model.TreeNode;
+
+import java.util.List;
+
+public class Solution0095 {
+    public List<TreeNode> generateTrees(int n) {
+        return null;
+    }
+}
