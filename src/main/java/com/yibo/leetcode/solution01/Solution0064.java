cb615b8bd4414dcdf2c3781d57a32ac484ec85c9
Bob Xu <Bob.Xu@nike.com>
2020-07-23 09:50:41 +0800

    最短路径和

--- src/main/java/com/yibo/leetcode/solution01/Solution0064.java
+++ src/main/java/com/yibo/leetcode/solution01/Solution0064.java
@@ -18,6 +18,18 @@ package com.yibo.leetcode.solution01;
  */
 public class Solution0064 {
     public int minPathSum(int[][] grid) {
-        return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[m - 1][n - 1] = grid[m - 1][n - 1];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j == n - 1) {
+                    continue;
+                }
+                dp[i][j] = Math.min(i == m - 1 ? Integer.MAX_VALUE : dp[i + 1][j], j == n - 1 ? Integer.MAX_VALUE : dp[i][j + 1]) + grid[i][j];
+            }
+        }
+        return dp[0][0];
     }
 }
