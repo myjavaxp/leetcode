fcded7d10c9d4b87479a7fc12e41dca81a1cfb1d
Bob Xu <Bob.Xu@nike.com>
2021-01-26 10:36:32 +0800

    0389

--- src/main/java/com/yibo/leetcode/solution01/Solution0389.java
+++ src/main/java/com/yibo/leetcode/solution01/Solution0389.java
@@ -1,16 +1,16 @@
 package com.yibo.leetcode.solution01;
 
 public class Solution0389 {
-    public char findTheDifference(String s, String t) {
-        int[] records = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            records[s.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < t.length(); i++) {
-            if (--records[t.charAt(i) - 'a'] < 0) {
-                return t.charAt(i);
-            }
-        }
-        return ' ';
-    }
+	public char findTheDifference(String s, String t) {
+		int[] records = new int[26];
+		for (int i = 0; i < s.length(); i++) {
+			records[s.charAt(i) - 'a']++;
+		}
+		for (int i = 0; i < t.length(); i++) {
+			if (--records[t.charAt(i) - 'a'] < 0) {
+				return t.charAt(i);
+			}
+		}
+		return ' ';
+	}
 }
